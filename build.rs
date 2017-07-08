47248ef4a1343cc233864675ed14c1074f5fef70
Luc Perkins <lucperkins@gmail.com>
2017-07-08 16:12:01 -0700

    get protobuf generation working

---
+++ build.rs
@@ -0,0 +1,10 @@
+extern crate protoc_rust_grpc;
+
+fn main() {
+    protoc_rust_grpc::run(protoc_rust_grpc::Args {
+        out_dir: "src/proto",
+        includes: &[],
+        input: &["proto/test.proto"],
+        rust_protobuf: true,
+    }).expect("protoc-rust-grpc");
+}
