605c6893d84626e372232a5bc4e86f066ba4a940
Luc Perkins <lucperkins@gmail.com>
2017-07-08 16:21:17 -0700

    add protobuf dependency for build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate protoc_rust_grpc;
 
 fn main() {
     protoc_rust_grpc::run(protoc_rust_grpc::Args {
-        out_dir: "src/proto",
+        out_dir: "proto",
         includes: &[],
         input: &["proto/test.proto"],
         rust_protobuf: true,
