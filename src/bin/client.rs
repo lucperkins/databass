04cbf8db24c57af679915df6fd358d6a3d0c5ee7
Luc Perkins <lucperkins@gmail.com>
2017-07-09 18:21:23 -0700

    remove protobuf installation from travis script

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,50 +2,54 @@ extern crate futures;
 extern crate grpc;
 extern crate lib;
 
+use grpc::{RequestOptions,SingleResponse};
 use lib::kv::{DeleteRequest,GetRequest,GetResponse,PutRequest,WriteResponse};
 use lib::kv_grpc::{KV,KVClient};
 
-use std::time;
+fn get(client: &KVClient, bucket: &str, key: &str) -> () {
+    let mut get_req: GetRequest = GetRequest::new();
+    get_req.set_bucket(String::from(bucket));
+    get_req.set_key(String::from(key));
+    let get_resp: SingleResponse<GetResponse> = client.get(RequestOptions::new(), get_req);
+    println!("{:?}", get_resp.wait());
+}
+
+fn put(client: &KVClient, bucket: &str, key: &str, value: &[u8]) -> () {
+    let mut put_req: PutRequest = PutRequest::new();
+    put_req.set_bucket(String::from(bucket));
+    put_req.set_key(String::from(key));
+    put_req.set_value(value.to_vec());
+    let put_resp: SingleResponse<WriteResponse> = client.put(RequestOptions::new(), put_req);
+    println!("{:?}", put_resp.wait());
+}
+
+fn delete(client: &KVClient, bucket: &str, key: &str) -> () {
+    let mut delete_req: DeleteRequest = DeleteRequest::new();
+    delete_req.set_bucket(String::from(bucket));
+    delete_req.set_key(String::from(key));
+    let delete_resp: SingleResponse<WriteResponse> = client.delete(RequestOptions::new(), delete_req);
+    println!("{:?}", delete_resp.wait());
+}
 
 fn main() {
+    let bucket = "my-bucket";
+    let key = "my-key";
     let client = KVClient::new_plain("localhost", 1111, Default::default()).unwrap();
-    let mut put_req = PutRequest::new();
-    put_req.set_bucket(String::from("my-bucket"));
-    put_req.set_key(String::from("my-key"));
-    put_req.set_value(String::from("Here is an initial value").into_bytes());
-
-    let put_resp: grpc::SingleResponse<WriteResponse> = client.put(grpc::RequestOptions::new(), put_req);
-    println!("{:?}", put_resp.wait());
-
-    let mut get_req = GetRequest::new();
-    get_req.set_bucket(String::from("my-bucket"));
-    get_req.set_key(String::from("my-key"));
-    let resp: grpc::SingleResponse<GetResponse> = client.get(grpc::RequestOptions::new(), get_req);
-    println!("{:?}", resp.wait());
-
-    let mut put_req2 = PutRequest::new();
-    put_req2.set_bucket(String::from("my-bucket"));
-    put_req2.set_key(String::from("my-key"));
-    put_req2.set_value(String::from("Here is yet another new value").into_bytes());
-
-    let put_resp2: grpc::SingleResponse<WriteResponse> = client.put(grpc::RequestOptions::new(), put_req2);
-    println!("{:?}", put_resp2.wait());
-
-    let mut get_req2 = GetRequest::new();
-    get_req2.set_bucket(String::from("my-bucket"));
-    get_req2.set_key(String::from("my-key"));
-    let resp2: grpc::SingleResponse<GetResponse> = client.get(grpc::RequestOptions::new(), get_req2);
-    println!("{:?}", resp2.wait());
-
-    let mut delete_req = DeleteRequest::new();
-    delete_req.set_bucket(String::from("my-bucket"));
-    delete_req.set_key(String::from("my-key"));
-    let resp3: grpc::SingleResponse<WriteResponse> = client.delete(grpc::RequestOptions::new(), delete_req);
-    println!("{:?}", resp3.wait());
-
-    let mut get_req3 = GetRequest::new();
-    get_req3.set_bucket(String::from("my-bucket"));
-    get_req3.set_key(String::from("my-key"));
-    let resp4: grpc::SingleResponse<GetResponse> = client.get(grpc::RequestOptions::new(), get_req3);
-    println!("{:?}", resp4.wait());
+    println!("Deleting...");
+    delete(&client, bucket, key);
+    println!("Failed get....");
+    get(&client, bucket, key);
+    println!("Putting...");
+    put(&client, bucket, key, "First value".as_bytes());
+    println!("Successful get...");
+    get(&client, bucket, key);
+    println!("Failed get...");
+    get(&client, "other-bucket", "other-key");
+    println!("Another put...");
+    put(&client, "other-bucket", "other-key", "Other value".as_bytes());
+    println!("Another successful get...");
+    get(&client, "other-bucket", "other-key");
+    println!("Cleaning up...");
+    delete(&client, bucket, key);
+    delete(&client, "other-bucket", "other-key");
 }
